a7f8f670b0ac4018478fcb2045b8094df01afbd7
Carlos <carlos.olmedodev@gmail.com>
2018-08-29 17:30:13 +0200

    Insert news subjects worked

--- private/query_functions.php
+++ private/query_functions.php
@@ -25,6 +25,27 @@
         return $subject; //return array assoc
     }
 
+    function insert_subject($menu_name, $position, $visible) {
+        global $db;
+
+        $sql = "INSERT INTO subjects (menu_name, position, visible) ";
+        $sql .= "VALUES (";
+        $sql .= "'" . $menu_name . "',";
+        $sql .= "'" . $position . "',";
+        $sql .= "'" . $visible . "'";
+        $sql .= ")";
+        $result = mysqli_query($db, $sql);
+        // For INSERT statements, $result is true/false
+        if ($result) {
+            return true;
+        } else {
+            // INSERT failed
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit;
+        }
+    }
+
     function find_all_pages() {
         global $db;
 
