fb69096ea127c5cfc7f82d141c6d633851786abc
Carlos <carlos.olmedodev@gmail.com>
2018-08-29 16:12:07 +0200

    Data from pages added & orded

--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -1,10 +1,6 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php
-
-$subject_set = find_all_subjects();
-
-?>
+<?php $subject_set = find_all_subjects(); ?>
 
 <?php $page_title = 'Subjects'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
